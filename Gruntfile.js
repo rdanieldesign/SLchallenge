613368eb9c201f59ca0e2c9c290c0ebebe5716f2
rdanieldesign <rdanieldesign@gmail.com>
2015-11-18 21:36:57 -0500

    remove svg renaming

--- Gruntfile.js
+++ Gruntfile.js
@@ -208,7 +208,6 @@ module.exports = function (grunt) {
         files: {
           src: [
             '<%= config.dist %>/styles/{,*/}*.css',
-            '<%= config.dist %>/images/{,*/}*.*',
             '<%= config.dist %>/*.{ico,png}'
           ]
         }
